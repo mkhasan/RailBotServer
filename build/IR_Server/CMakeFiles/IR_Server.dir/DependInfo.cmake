756b403f0dd10dbfacada7421f0ba0e87d1f1869
mkhasan <hasan@kaist.ac.kr>
2018-07-02 02:12:14 -0400

    restructured

---
+++ build/IR_Server/CMakeFiles/IR_Server.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/usrc/RailBotServer/IR_Server/src/CRtspSession.cpp" "/home/usrc/RailBotServer/build/IR_Server/CMakeFiles/IR_Server.dir/src/CRtspSession.cpp.o"
+  "/home/usrc/RailBotServer/IR_Server/src/CStreamer.cpp" "/home/usrc/RailBotServer/build/IR_Server/CMakeFiles/IR_Server.dir/src/CStreamer.cpp.o"
+  "/home/usrc/RailBotServer/IR_Server/src/IR_Server.cpp" "/home/usrc/RailBotServer/build/IR_Server/CMakeFiles/IR_Server.dir/src/IR_Server.cpp.o"
+  "/home/usrc/RailBotServer/IR_Server/src/JPEGSamples.cpp" "/home/usrc/RailBotServer/build/IR_Server/CMakeFiles/IR_Server.dir/src/JPEGSamples.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
