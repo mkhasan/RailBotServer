0da06d06205bf66751a070b18f7424044cf24849
Hasan <hasan@kaist.ac.kr>
2018-07-16 10:54:59 +0900

    utils_test.cpp has been modified in win

--- Utils/src/utils_test.cpp
+++ Utils/src/utils_test.cpp
@@ -20,26 +20,34 @@ int main(int argc, char **argv) {
 	const char *str = "This is a test.This is a test.This is a test.1234567This is a test.This is a test.123456";
 	Squeezer *p = Squeezer::Instance();
 
-	int length = strlen(str);
+	
+	size_t length = strlen(str);
 	char dest[1024];
-	int destLength = sizeof(dest);
+	size_t destLength = sizeof(dest);
+	size_t decomSize = 1024;
+	
 
-	int ret = p->Compress(dest, destLength, str, length);
+	int ret = p->Initialize();
+	if (ret != 0) {
+		printf("Error in initializing...ret val %d \n", ret);
+		return 0;
+	}
 
-	//cout << "Length is " << length << endl;
+	ret = p->Compress((lzham_uint8 *) dest, &destLength, (const lzham_uint8 *)str, length);
 
 	printf("Length is %d ret value %d \n", destLength, ret);
 
-	char temp[1024], decom[1024];
+	char temp[1024];
+	char decom[1024];
 
-	for(int i=0; i<destLength+1; i++)
-	 	temp[i] = dest[i];
+	for (int i = 0; i<destLength + 1; i++)
+		temp[i] = dest[i];
 
-	int decomSize = 1024;
-	ret = p->DeCompress(decom, decomSize, temp, destLength);
+	
+	ret = p->DeCompress((lzham_uint8 *)decom, &decomSize, (const lzham_uint8 *)temp, destLength);
 
 	printf("Decom Length is %d ret value %d \n", decomSize, ret);
-	for(int i=0; i<decomSize; i++)
+	for (int i = 0; i<decomSize; i++)
 		printf("%c", decom[i]);
 	printf("\n");
 
